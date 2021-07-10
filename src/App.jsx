8cca4ee2059cb48bf9135951019627b690d1e01d
Christopher Nugroho <Chrisnulis@gmail.com>
2021-07-09 23:36:38 -0400

    Topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import Topbar from "./components/topbar/Topbar";
 import logo from './logo.svg';
 import './app.scss';
 
 function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="App">
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       HELLO
     </div>
   );
