ead7c2db2e275d4ea94f3621c161a8136da6910e
Christopher Nugroho <Chrisnulis@gmail.com>
2021-07-09 23:08:42 -0400

    Bug Hunting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Topbar from "./components/topbar/Topbar";
+import Menu from "./components/menu/Menu";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import "./app.scss"
+import { useState } from "react";
+
+
+
 
 function App() {
   return (
