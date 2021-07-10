e56f7e3214017f6da38d81e21a6891afac6a90ee
Christopher Nugroho <Chrisnulis@gmail.com>
2021-07-09 23:51:19 -0400

    IT IS WORKING

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
 import Topbar from "./components/topbar/Topbar";
-import logo from './logo.svg';
-import './app.scss';
+import Menu from "./components/menu/Menu";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import "./app.scss"
+import { useState } from "react";
+
+
+
+
 
 function App() {
   const [menuOpen,setMenuOpen] = useState(false)
   return (
-    <div className="App">
+    <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      HELLO
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <div className = "sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
     </div>
-  );
-}
+    )
+  }
+
 
 export default App;
